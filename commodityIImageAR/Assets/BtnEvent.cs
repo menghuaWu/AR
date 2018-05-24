e6b03ab27cca6064e20b9f83372ab06f20c46ab0
menghuaWu <boy199604@gmail.com>
2018-05-24 09:49:08 +0800

    20180524

---
+++ commodityIImageAR/Assets/BtnEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnEvent : MonoBehaviour {
+
+    
+
+	public void OnOpenURL(string URL)
+    {
+        Application.OpenURL(URL);
+    }
+}
